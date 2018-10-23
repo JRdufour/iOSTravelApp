42481e4c9e1ce36e54936fa2d7a3d0b2675db004
Jonathan Dufour <Jonathan.Dufour85@stclairconnect.ca>
2018-10-23 17:49:13 -0400

    Initial Commit

---
+++ TravelApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TravelApp
+//
+//  Created by jonathan Dufour on 2018-10-23.
+//  Copyright © 2018 ca.stclairconnect.JonathanDufour. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
