70084fe43b3752898354bea3b951db4ad182c3cd
Jonathan Dufour <Jonathan.Dufour85@stclairconnect.ca>
2018-10-23 19:32:57 -0400

    added google places api with key

--- TravelApp/AppDelegate.swift
+++ TravelApp/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        GMSPlacesClient.provideAPIKey("AIzaSyBZfCX_8bykLxEpzLPMduV2MlJ9VaeKsxI")
+        
+        
         return true
     }
 
--- TravelApp/TripsListViewController.swift
+++ TravelApp/TripsListViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GooglePlaces
 
 class TripsListViewController: UIViewController {
 
@@ -20,10 +21,45 @@ class TripsListViewController: UIViewController {
 
     @objc func addTrip(){
         //add code to add a trip
+        let autocompleteController = GMSAutocompleteViewController()
+        autocompleteController.delegate = self
+        present(autocompleteController, animated: true, completion: nil)
         
     }
 
 }
 
+extension TripsListViewController: GMSAutocompleteViewControllerDelegate{
+  //handle the user's selection.
+    
+    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+        print("Place name: \(place.name)")
+        print("Place address: \(place.formattedAddress)")
+        print("Place attributions: \(place.attributions)")
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+        // TODO: handle the error.
+        print("Error: ", error.localizedDescription)
+    }
+    
+    // User canceled the operation.
+    func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    // Turn the network activity indicator on and off again.
+    func didRequestAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+    }
+    
+    func didUpdateAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+    }
+
+
+
+}
 
 
