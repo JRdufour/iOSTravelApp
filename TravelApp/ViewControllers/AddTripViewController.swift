1ddc77cc8cf945ed66de874c47391bc425e5904e
Jonathan Dufour <Jonathan.Dufour85@stclairconnect.ca>
2018-11-11 13:24:18 -0500

    more GUI

--- TravelApp/ViewControllers/AddTripViewController.swift
+++ TravelApp/ViewControllers/AddTripViewController.swift
@@ -8,20 +8,30 @@
 
 import UIKit
 
-class AddTripViewController: UIViewController {
-
+class AddTripViewController: UITableViewController {
+    //MARK: Outlets
+    @IBOutlet var tripNameTextField: UITextField!
+    @IBOutlet var startDatePicker: UIDatePicker!
+    @IBOutlet var endDatePicker: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "Add a trip"
+        self.title = "New Trip"
         // Do any additional setup after loading the view.
     }
     
     @IBAction func AddTrip(_ sender: Any) {
     //    print("trip added")
         print("trip added")
+        //TODO add trip to the database
+        
+        
         self.dismiss(animated: true, completion: nil)
     }
     
+    @IBAction func cancelTrip(_ sender: Any) {
+         self.dismiss(animated: true, completion: nil)
+    }
     
     /*
     // MARK: - Navigation
